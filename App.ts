b6b44261fbb7fc6694806f9d8d98b6a92f14ba22
Eduardo <eduardomacielbr@gmail.com>
2022-06-18 23:41:51 -0300

    Updated app to use CORS

--- App.ts
+++ App.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import cors from "cors";
 
 require('dotenv').config()
 
 import { router } from "./api.routes";
 const app = express();
+
+app.use(cors())
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
